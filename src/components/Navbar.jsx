b7a70202b5c1d07c9c55d6fb79bca19be05d187d
shalini <s.rathore43@gmail.com>
2025-10-04 17:54:45 +0530

    asas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router'
 export default function Navbar() {
   return (
   
-<nav className="relative bg-blue-800">
+<nav className="relative bg-grey-800">
   <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div className="relative flex h-16 items-center justify-between">
       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
